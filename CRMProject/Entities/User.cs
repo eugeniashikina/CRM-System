6e0f7d9d858030d90bf1bcdeba8b1392f31a03f4
eugeniashikina <72591234+eugeniashikina@users.noreply.github.com>
2021-12-05 18:39:21 +0300

    Добавила сущность пользователя

---
+++ CRMProject/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿namespace CRMProject.Entity
+{
+    /// <summary>
+    ///  Сущность пользователя
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Идентификатор пользователя
+        /// </summary>
+        public static long Id { get; set; }
+
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        public static string Login { get; set; }
+
+        /// <summary>
+        /// Пароль пользователя
+        /// </summary>
+        public static string Password { get; set; }
+
+        /// <summary>
+        /// Роль пользователя
+        /// </summary>
+        public static string Role { get; set; }
+    }
+}
