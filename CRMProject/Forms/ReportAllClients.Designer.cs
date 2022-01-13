156413efd7b065522afe1d637775ba217607dc9d
eugeniashikina <Shik-310101@mail.ru>
2022-01-13 20:45:18 +0300

    Диаграмма по клиентам и сумме

--- CRMProject/Forms/ReportAllClients.Designer.cs
+++ CRMProject/Forms/ReportAllClients.Designer.cs
@@ -28,11 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.Report = new System.Windows.Forms.DataGridView();
             this.Id_client = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CountOrders = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.SellMoney = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.Report)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).BeginInit();
             this.SuspendLayout();
             // 
             // Report
@@ -47,10 +52,9 @@
             this.Id_client,
             this.CountOrders,
             this.SellMoney});
-            this.Report.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Report.Location = new System.Drawing.Point(0, 0);
             this.Report.Name = "Report";
-            this.Report.Size = new System.Drawing.Size(559, 229);
+            this.Report.Size = new System.Drawing.Size(559, 300);
             this.Report.TabIndex = 1;
             // 
             // Id_client
@@ -68,15 +72,33 @@
             this.SellMoney.HeaderText = "Всего потрачено денег";
             this.SellMoney.Name = "SellMoney";
             // 
+            // Chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Chart.Legends.Add(legend1);
+            this.Chart.Location = new System.Drawing.Point(565, 0);
+            this.Chart.Name = "Chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Клиенты";
+            this.Chart.Series.Add(series1);
+            this.Chart.Size = new System.Drawing.Size(300, 300);
+            this.Chart.TabIndex = 2;
+            this.Chart.Text = " Диаграмма";
+            // 
             // ReportAllClients
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(559, 229);
+            this.ClientSize = new System.Drawing.Size(866, 300);
+            this.Controls.Add(this.Chart);
             this.Controls.Add(this.Report);
             this.Name = "ReportAllClients";
             this.Text = "ReportAllClients";
             ((System.ComponentModel.ISupportInitialize)(this.Report)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -87,5 +109,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Id_client;
         private System.Windows.Forms.DataGridViewTextBoxColumn CountOrders;
         private System.Windows.Forms.DataGridViewTextBoxColumn SellMoney;
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart;
     }
 }
--- CRMProject/Forms/ReportAllClients.cs
+++ CRMProject/Forms/ReportAllClients.cs
@@ -1,5 +1,7 @@
 ﻿using CRMProject.Contollers;
 using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
 
 namespace CRMProject.Forms
 {
@@ -10,6 +12,27 @@ namespace CRMProject.Forms
             InitializeComponent();
             ControllerReports reports = new ControllerReports();
             Report = reports.GetInfoAboutOrdersAllClints(Report);
+            SetDataChart();
+        }
+
+        /// <summary>
+        ///     Вставляет данные в граффик.
+        /// </summary>
+        private void SetDataChart()
+        {
+            List<string> nameClients = new List<string>();
+            List<string> sellMoney = new List<string>();
+
+            for (int i = 0; i < Report.Rows.Count; i++)
+            {
+                nameClients.Add(Report.Rows[i].Cells["Id_client"].Value.ToString());
+                sellMoney.Add(Report.Rows[i].Cells["SellMoney"].Value.ToString());
+            }
+
+            for (int i = 0; i < nameClients.Count && i < sellMoney.Count; i++)
+            {
+                Chart.Series["Клиенты"].Points.AddXY(nameClients[i], Convert.ToDouble(sellMoney[i]));
+            }
         }
     }
 }
